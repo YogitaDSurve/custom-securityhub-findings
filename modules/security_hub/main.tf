88969fc7cc3b2948a179e1a8027478d6a385366d
Yogita Surve <yogita.surve@medly.com>
2022-01-27 11:43:05 +0530

    Enable security hub for only one account

--- modules/security_hub/main.tf
+++ modules/security_hub/main.tf
@@ -1,11 +1,1 @@
-locals {
-  securityhub_standards_control            = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account_id.account_id}"
-}
-
-resource "aws_securityhub_organization_configuration" "enable_security_hub_for_organization" {
-  auto_enable = true
-}
-
-data "aws_region" "current_region" {}
-data "aws_caller_identity" "current_account_id" {}
-
+resource "aws_securityhub_account" "enable_security_hub" {}
